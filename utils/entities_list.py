1ac01d48c0a5f8c9690b2b5f8ee9c142d14b70c2
q1025311 <imon.seth@quintiles.com>
2022-05-05 15:43:32 +0530

    borrowed and updated for custom

---
+++ utils/entities_list.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# @Author: Wenwen Yu
+# @Created Time: 7/8/2020 9:34 PM
+
+Entities_list = [
+    "header",
+    "keys",
+    "values"
+]
+
+# Entities_list = [
+#     "h_key",
+#     "h_answer"
+# ]
+
+# Entities_list = [
+#     "name_of_clinical_investigator",
+#     "curriculum_vitae",
+#     "other_statement",
+#     "name_of_medical_school",
+#     "name_of_clinical_laboratory",
+#     "name_of_irb",
+#     "names_of_subinvestigators",
+#     "name_and_code_number",
+#     "address_1",
+#     "address_2",
+#     "city",
+#     "state",
+#     "country",
+#     "zip_code"
+# ]
+
+# Entities_list = [
+#     "company",
+#     "address",
+#     "date",
+#     "total"
+# ]
+
+# Entities_list = [
+#     "ticket_num",
+#     "starting_station",
+#     "train_num",
+#     "destination_station",
+#     "date",
+#     "ticket_rates",
+#     "seat_category",
+#     "name"
+# ]
